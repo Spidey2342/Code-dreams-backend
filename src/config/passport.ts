473f13647d4a782f2bccd95a1b3b717c7f27db74
Spidey2342 <mygoals990@gmail.com>
2026-06-01 23:38:36 +0000

    as

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,7 +1,7 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as GitHubStrategy } from "passport-github2";
-import { prisma } from "../lib/prisma";
+import { prisma } from "../../lib/prisma";
 
 passport.use(
   new GoogleStrategy(
